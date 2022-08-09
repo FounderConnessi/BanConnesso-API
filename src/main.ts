29a38a7a69aaf191a9b8454ab9937dcc37d263e3
MrFreasy <46691191+MrFreasy@users.noreply.github.com>
2022-08-09 15:02:09 +0200

    Revert "Aggiunto pacchetto helmet"
    This reverts commit a779648b376d464229ec98b90e59ff195d95fc18.

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.use(helmet());
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true
   }));
