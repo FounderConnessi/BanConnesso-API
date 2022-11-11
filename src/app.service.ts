d1f60e965a9d78aa9952627c6d75fe9685357336
MrFreasy <46691191+MrFreasy@users.noreply.github.com>
2022-11-11 13:42:48 +0100

    Aggiunta versione del plugin per il controllo degli aggiornamenti

--- src/app.service.ts
+++ src/app.service.ts
@@ -8,7 +8,12 @@ export class AppService {
     return {
       status: "online",
       environment: process.env.NODE_ENV,
-      version: process.env.npm_package_version,
+      version: {
+        api: process.env.npm_package_version,
+        bungeecord: process.env.bungeecord_version,
+        velocity: process.env.velocity_version,
+        spigot: process.env.spigot_version
+      },
       uptime: UtilsDate.secondsToFormattedStringEN(
         Math.trunc(process.uptime())
       ), 
